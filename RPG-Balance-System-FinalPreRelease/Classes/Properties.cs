77199cb3d4b63aba751bc5e232c8ef19cc5610d9
Fernando Almendro Martí <72808136+FernaToty@users.noreply.github.com>
2022-03-06 10:55:33 +0100

    Balancing system
    Searching information for the balancing system, I found one balancing system creator that can be really useful, I've made some little changes, I've you want to see the original one, go to WeylonSantana github page.

---
+++ RPG-Balance-System-FinalPreRelease/Classes/Properties.cs
@@ -0,0 +1,41 @@
+﻿namespace RPGBalanceSystem.Classes
+{
+    public static class Properties
+    {
+        public static int PlyrLevel;
+        public static int PlyrAttack;
+        public static int PlyrDefese;
+        public static int PlyrMagicAttack;
+        public static int PlyrMagicDefense;
+        public static int PlyrSpeed;
+        public static int PlyrHpBase;
+        public static double PlyrHpFac;
+        public static int PlyrExpBase;
+        public static double PlyrExpFac;
+        public static int PlyrPoints;
+        public static int PlyrMaxPoints;
+        public static int PlyrBaseDmg;
+        public static int PlyrScalingStat;
+        public static double PlyrScalingFac;
+        public static double PlyrCriticalFac;
+        public static bool Balanced;
+        public static bool Strong;
+        public static bool Tanker;
+
+        public static double EnyAttack;
+        public static double EnyDefese;
+        public static double EnyMagicAttack;
+        public static double EnyMagicDefense;
+        public static double EnySpeed;
+        public static double EnyHpFac;
+        public static int EnyBaseDmg;
+        public static int EnyScalingStat;
+        public static double EnyScalingFac;
+        public static double EnyCriticalFac;
+
+        public static string MinTrueFormula;
+        public static string MaxTrueFormula;
+        public static string MinRealFormula;
+        public static string MaxRealFormula;
+    }
+}
